351c5c17cbde1ac796f9874f7509ed9779721d3e
Paula Sousa <paulasousa137@gmail.com>
2023-04-19 15:41:00 +0200

    created basic dao

---
+++ src/main/java/com/example/demo/dao/IJoinDao.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.dto.Join;
+
+
+public interface IJoinDao extends JpaRepository<Join, Integer>{
+
+}
